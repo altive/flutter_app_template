c3f6db94fedb5e4be4394f2d1abc203e967595a4
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2021-08-15 20:10:32 +0900

    Add build_runner, json_serializable

---
+++ lib/commons/json_converter/timestamp_converter.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+class TimestampConverter implements JsonConverter<DateTime?, Timestamp?> {
+  const TimestampConverter();
+
+  @override
+  DateTime? fromJson(Timestamp? json) => json?.toDate();
+
+  @override
+  Timestamp? toJson(DateTime? object) =>
+      object == null ? null : Timestamp.fromDate(object);
+}
