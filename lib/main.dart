389a79ee8433ba8b86c49acdbb8f618fd4dcbad7
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2021-08-15 17:40:53 +0900

    Add firebase_core for Android, iOS, Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'presentation/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Future.wait([
+    Future(Firebase.initializeApp),
+  ]);
+
   runApp(
     const ProviderScope(
       child: App(),
