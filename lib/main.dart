42cacc4d183e913ac7bf3a823dd9a04993785633
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2021-08-15 19:22:43 +0900

    Add shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'commons/providers/shared_preferences_provider.dart';
 import 'presentation/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  late final SharedPreferences sp;
   await Future.wait([
     Future(Firebase.initializeApp),
+    Future(() async => sp = await SharedPreferences.getInstance()),
   ]);
 
   runApp(
-    const ProviderScope(
-      child: App(),
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sp),
+      ],
+      child: const App(),
     ),
   );
 }
