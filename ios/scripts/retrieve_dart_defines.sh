7e764b918527859a65075059d324162d00057957
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2021-08-29 07:59:58 +0900

    Add flavor.xcconfig, lightweight retrieve_dart_defines.sh

--- ios/scripts/retrieve_dart_defines.sh
+++ ios/scripts/retrieve_dart_defines.sh
@@ -3,7 +3,7 @@
 # 作成ファイルパス
 OUTPUT_FILE="${SRCROOT}/Flutter/DartDefines.xcconfig"
 
-# Flutter 2.2 以降で必要な、Dart-Defineの変換処理 
+# Flutter 2.2 以降で必要な、Dart-Defineのデコード処理 
 function decode_url() { echo "${*}" | base64 --decode; }
 
 # あらかじめファイル内容を空にする
@@ -13,19 +13,13 @@ IFS=',' read -r -a define_items <<<"$DART_DEFINES"
 
 for index in "${!define_items[@]}"
 do
-    # Flutter 2.2 以降で必要なエンコードを実行する
+    # Flutter 2.2 以降で必要なデコードを実行する
     item=$(decode_url "${define_items[$index]}")
     # FLAVORが含まれるDart Defineの場合
     if [ $(echo $item | grep 'FLAVOR') ] ; then
-        # XCConfigファイルに書き込み
-        echo $item >> $OUTPUT_FILE
-        # FLAVORの値
+        # FLAVORの値(=の右側)
         value=${item#*=}
-        # AppIcon用Suffix
-        echo ICON_SUFFIX=-${value} >> $OUTPUT_FILE
-        # Production環境以外の場合にBundle IDのSuffixを設定する
-        if [ $(echo $item | grep -v 'prod') ] ; then
-            echo FLAVOR_SUFFIX=.${value} >> $OUTPUT_FILE
-        fi
+        # FLAVORに対応したXCConfigファイルをincludeさせる
+        echo "#include \"$value.xcconfig\"" >> $OUTPUT_FILE
     fi
 done
