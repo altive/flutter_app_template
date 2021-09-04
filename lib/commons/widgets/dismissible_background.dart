afb8f366e651e9c73a8136b064586ff84cf20565
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2021-09-04 18:52:35 +0900

    Add DismissibleBackground

---
+++ lib/commons/widgets/dismissible_background.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+/// スワイプで削除できるアイテムのWidget
+class DismissibleBackground extends StatelessWidget {
+  // Constructor
+  const DismissibleBackground({
+    Key? key,
+    required this.child,
+    this.backgroundColor = Colors.red,
+    this.margin = EdgeInsets.zero,
+    this.alignment = Alignment.centerRight,
+    this.borderRadius = BorderRadius.zero,
+  }) : super(key: key);
+
+  /// 表示する文字列。デフォルトは `Delete`
+  final Widget child;
+  final Color backgroundColor;
+  final EdgeInsetsGeometry margin;
+  final Alignment alignment;
+  // どの角にどのくらいの角丸を施すか
+  final BorderRadius borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      alignment: alignment,
+      margin: margin,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: borderRadius,
+      ),
+      child: child,
+    );
+  }
+}
