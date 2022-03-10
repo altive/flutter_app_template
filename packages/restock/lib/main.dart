c5fdfc0c821a5751b1877a3929d1d518d23164ce
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2022-03-10 11:13:41 +0900

    Add flutter_dotenv

--- packages/restock/lib/main.dart
+++ packages/restock/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:enum_to_string/enum_to_string.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,19 +11,14 @@ import 'src/utils/utils.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // ビルド時に指定したFlavorを取得
-  final flavor = EnumToString.fromString(
-    Flavor.values,
-    const String.fromEnvironment('FLAVOR'),
-  )!;
-  logger.fine('Flutter App Run Started by Flavor: $flavor');
 
-  // Firebaseの初期化
-  // Firebaseの各サービスを使う前に初期化を済ませておく必要がある
-  await Firebase.initializeApp();
+  final flavor = Flavor.values.byName(const String.fromEnvironment('FLAVOR'));
+
+  await Future.wait<void>([
+    Firebase.initializeApp(),
+  ]);
 
   return runApp(
-    // ここ以下でProviderが使用可能
     ProviderScope(
       overrides: [
         flavorProvider.overrideWithValue(flavor),
