36dbae1d79ce716bb291efc40091b376a5c466f5
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2022-03-27 06:21:24 +0900

    Remove api_store package

--- packages/api_store/lib/api_store.dart
+++
@@ -1,1 +0,0 @@
-library api_store;
--- packages/api_store/lib/src/api_request/http_methods/http_methods.dart
+++
@@ -1,5 +0,0 @@
-export 'delete_api_request.dart';
-export 'get_api_request.dart';
-export 'patch_api_request.dart';
-export 'post_api_request.dart';
-export 'put_api_request.dart';
