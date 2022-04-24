f5099736ba3a8fbdcd6df928bccb31865ac55247
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2022-04-24 16:54:35 +0900

    chore: remove unused altive_flutter_kit package

--- packages/ranking_note_app/lib/presentation/app_info_page/app_info_page.dart
+++ packages/ranking_note_app/lib/presentation/app_info_page/app_info_page.dart
@@ -1,4 +1,3 @@
-import 'package:altive_flutter_kit/altive_flutter_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
