4e6f83532f9d1cce4d78b1d4e66959b582e3d773
Muramatsu Ryunosuke <muramatsu@altive.co.jp>
2022-09-07 02:58:42 +0900

    build: FirebaseSDKVersion = '9.4.0'

--- packages/ranking_note_app/lib/commons/providers/firebase_dynamic_links_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-/// Firebase Dynamic Linksのインスタンスを提供する
-final dynamicLinksProvider = Provider((ref) => FirebaseDynamicLinks.instance);
