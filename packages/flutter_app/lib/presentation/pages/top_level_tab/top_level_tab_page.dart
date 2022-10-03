48653b6cedfa1fc7c04773c7e30da25fbf32d3cb
Muramatsu Ryunosuke <muramatsu@altive.co.jp>
2022-10-03 13:18:41 +0900

    chore: remove unused files

--- packages/flutter_app/lib/presentation/pages/top_level_tab/top_level_tab_page.dart
+++ packages/flutter_app/lib/presentation/pages/top_level_tab/top_level_tab_page.dart
@@ -1,9 +1,9 @@
+import 'package:convenient_widgets/convenient_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../commons/widgets/unfocus_on_tap.dart';
 import '../../../util/localizer/localizer.dart';
 import 'top_level_tab.dart';
 import 'top_level_tab_provider.dart';
