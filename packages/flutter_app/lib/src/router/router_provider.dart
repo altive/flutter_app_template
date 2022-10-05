d2e3a2ce19be4d4b37e21c64a158de58681f4179
Muramatsu Ryunosuke <muramatsu@altive.co.jp>
2022-10-05 16:05:50 +0900

    chore(flutter_app): bump go_router to v5

--- packages/flutter_app/lib/src/router/router_provider.dart
+++ packages/flutter_app/lib/src/router/router_provider.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -17,25 +15,5 @@ GoRouter router(RouterRef ref) {
       location: state.location,
       exception: state.error!,
     ).build(context),
-    navigatorBuilder: (context, state, child) {
-      if (kReleaseMode) {
-        return const SizedBox();
-      } else {
-        return Material(
-          child: SafeArea(
-            child: Column(
-              children: [
-                Expanded(child: child),
-                const Divider(height: 1),
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Text('Route location: ${state.location}'),
-                ),
-              ],
-            ),
-          ),
-        );
-      }
-    },
   );
 }
