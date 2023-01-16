ef7ba42af41b68b89b3980771fa02c37cd326074
Muramatsu Ryunosuke <muramatsu@altive.co.jp>
2023-01-16 13:45:46 +0900

    chore: TopLevelTab to Enhanced enum

--- packages/flutter_app/lib/src/pages/top_level_tab/top_level_tab.dart
+++ packages/flutter_app/lib/src/pages/top_level_tab/top_level_tab.dart
@@ -5,38 +5,31 @@ import '../home_page/home_page.dart';
 import '../riverpod_example_page/riverpod_example_page.dart';
 import '../settings_page/settings_page.dart';
 
-// TODO(Riscait): 本当はEnhanced enumにしたかったが、
-// go_router_builderがEnhanced Enumに対応していないため使用できず
-// Issue: https://github.com/flutter/flutter/issues/105876
 enum TopLevelTab {
-  home,
-  riverpod,
-  settings,
+  home(
+    iconData: Icons.home,
+    page: HomePage(),
+  ),
+  riverpod(
+    iconData: Icons.home,
+    page: RiverpodExamplePage(),
+  ),
+  settings(
+    iconData: Icons.home,
+    page: SettingsPage(),
+  ),
+  ;
+
+  const TopLevelTab({
+    required this.iconData,
+    required this.page,
+  });
+
+  final IconData iconData;
+  final Widget page;
 }
 
 extension TopLevelTabExt on TopLevelTab {
-  IconData get iconData {
-    switch (this) {
-      case TopLevelTab.home:
-        return Icons.home;
-      case TopLevelTab.riverpod:
-        return Icons.stream;
-      case TopLevelTab.settings:
-        return Icons.settings;
-    }
-  }
-
-  Widget get page {
-    switch (this) {
-      case TopLevelTab.home:
-        return const HomePage();
-      case TopLevelTab.riverpod:
-        return const RiverpodExamplePage();
-      case TopLevelTab.settings:
-        return const SettingsPage();
-    }
-  }
-
   String labelText(AppLocalizations l10n) {
     switch (this) {
       case TopLevelTab.home:
