63f4ea9e4fcc141843bb901a10ff97af7477eee6
Muramatsu Ryunosuke <muramatsu@altive.co.jp>
2023-03-07 07:36:49 +0900

    chore: update bubble_border widget

--- packages/convenient_widgets/lib/src/bubble_border.dart
+++ packages/convenient_widgets/lib/src/bubble_border.dart
@@ -9,8 +9,6 @@ enum Destination {
 
 /// [ShapeDecoration]の`shape`パラメータで使用できる
 /// [destination]は吹き出しの向き
-/// Llftは使用可能、rightはRect.fromPointsの値等を調整する必要あり
-/// top, bottomは未確認
 class BubbleBorder extends ShapeBorder {
   const BubbleBorder({
     this.destination = Destination.left,
@@ -84,21 +82,21 @@ class BubbleBorder extends ShapeBorder {
         path
           ..addRRect(RRect.fromRectAndRadius(r, const Radius.circular(8)))
           // 始点の調整
-          ..moveTo(r.centerRight.dx, r.centerRight.dy - 10)
+          ..moveTo(r.topCenter.dx - 12, r.topCenter.dy)
+          // 右上へ直線を描く
+          ..relativeLineTo(12, -12)
           // 右下へ直線を描く
-          ..relativeLineTo(12, 12)
-          // 左下へ直線を描く
-          ..relativeLineTo(-12, 12);
+          ..relativeLineTo(12, 12);
         break;
       case Destination.bottom:
         path
           ..addRRect(RRect.fromRectAndRadius(r, const Radius.circular(8)))
           // 始点の調整
-          ..moveTo(r.centerRight.dx, r.centerRight.dy - 10)
-          // 右下へ直線を描く
-          ..relativeLineTo(12, 12)
+          ..moveTo(r.bottomCenter.dx + 12, r.bottomCenter.dy)
           // 左下へ直線を描く
-          ..relativeLineTo(-12, 12);
+          ..relativeLineTo(-12, 12)
+          // 左上へ直線を描く
+          ..relativeLineTo(-12, -12);
         break;
     }
     // 始点まで線を繋げて閉じる
