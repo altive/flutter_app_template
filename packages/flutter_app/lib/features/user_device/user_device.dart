666966ab1cba802c71878f84512b8ab27ed24bd5
Muramatsu Ryunosuke <muramatsu@altive.co.jp>
2023-05-19 16:10:55 +0900

    chore: プライベートな_DeviceOsを削除して推奨のbool値を使用

--- packages/flutter_app/lib/features/user_device/user_device.dart
+++ packages/flutter_app/lib/features/user_device/user_device.dart
@@ -7,43 +7,37 @@ import 'model/user_device.dart';
 export 'model/user_device.dart';
 export 'user_device_provider.dart';
 
-/// 実行デバイスのOperatingSystem。アプリが動作対象とするもののみを列挙。
-enum _DeviceOs {
-  android,
-  ios,
-  macos,
-}
-
 Future<UserDevice> retrieveUserDevice() async {
-  final os = Platform.operatingSystem;
-  final deviceOs = _DeviceOs.values.byName(os);
-
   final deviceInfoPlugin = DeviceInfoPlugin();
 
-  switch (deviceOs) {
-    case _DeviceOs.android:
-      final info = await deviceInfoPlugin.androidInfo;
-      return UserDevice.android(
-        name: '${info.manufacturer} ${info.model}',
-        model: info.model,
-        osName: info.version.codename,
-        osVersionString: info.version.release,
-      );
-    case _DeviceOs.ios:
-      final info = await deviceInfoPlugin.iosInfo;
-      return UserDevice.ios(
-        name: info.utsname.machine,
-        model: info.model,
-        osName: info.systemName,
-        osVersionString: info.systemVersion,
-      );
-    case _DeviceOs.macos:
-      final info = await deviceInfoPlugin.macOsInfo;
-      return UserDevice.macos(
-        name: info.computerName,
-        model: info.model,
-        osName: info.osRelease,
-        osVersionString: info.kernelVersion,
-      );
+  if (Platform.isAndroid) {
+    final info = await deviceInfoPlugin.androidInfo;
+    return UserDevice.android(
+      name: '${info.manufacturer} ${info.model}',
+      model: info.model,
+      osName: info.version.codename,
+      osVersionString: info.version.release,
+    );
   }
+
+  if (Platform.isIOS) {
+    final info = await deviceInfoPlugin.iosInfo;
+    return UserDevice.ios(
+      name: info.utsname.machine,
+      model: info.model,
+      osName: info.systemName,
+      osVersionString: info.systemVersion,
+    );
+  }
+
+  if (Platform.isMacOS) {
+    final info = await deviceInfoPlugin.macOsInfo;
+    return UserDevice.macos(
+      name: info.computerName,
+      model: info.model,
+      osName: info.osRelease,
+      osVersionString: info.kernelVersion,
+    );
+  }
+  throw UnimplementedError('Unsupported platform');
 }
