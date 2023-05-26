4fc25cd37ffdea5f914d528849f4d6c17af92057
naipaka <naipakapaka@gmail.com>
2023-05-26 10:32:35 +0900

    refactor: flutter_appパッケージ内のProviderをGenerator Providerに置き換え

---
+++ packages/flutter_app/lib/features/theme_selector/theme_selector_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+
+part of 'theme_selector_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$themeSelectorHash() => r'ebfc6bd18dd15fad233997c72389ea2a4291c0a0';
+
+/// See also [ThemeSelector].
+@ProviderFor(ThemeSelector)
+final themeSelectorProvider =
+    NotifierProvider<ThemeSelector, ThemeMode>.internal(
+  ThemeSelector.new,
+  name: r'themeSelectorProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$themeSelectorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ThemeSelector = Notifier<ThemeMode>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
---
+++ packages/flutter_app/lib/features/validator/validator_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+
+part of 'validator_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$validatorHash() => r'0e3284305da341e42bc0f622cbf585f28e45e9c0';
+
+/// See also [validator].
+@ProviderFor(validator)
+final validatorProvider = Provider<Validator>.internal(
+  validator,
+  name: r'validatorProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$validatorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ValidatorRef = ProviderRef<Validator>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
