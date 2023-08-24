c9b8ece4adc96484cee46df0500ae8c107450880
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2023-08-24 14:30:06 +0900

    build: Upgrade packages and build codes (#237)
    * build: upgrade packages
    
    * add duplicate_ignore to ignore_for_file
    
    * build: generate codes with build_runner

--- packages/authenticator/lib/src/apple_authenticator.g.dart
+++ packages/authenticator/lib/src/apple_authenticator.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+// ignore_for_file: duplicate_ignore, type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
 
 part of 'apple_authenticator.dart';
 
@@ -24,4 +24,5 @@ final appleAuthenticatorProvider = Provider<AppleAuthenticator>.internal(
 );
 
 typedef AppleAuthenticatorRef = ProviderRef<AppleAuthenticator>;
-// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
--- packages/remote_parameter_fetcher/lib/src/remote_parameter_fetcher_provider.g.dart
+++ packages/remote_parameter_fetcher/lib/src/remote_parameter_fetcher_provider.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+// ignore_for_file: duplicate_ignore, type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
 
 part of 'remote_parameter_fetcher_provider.dart';
 
@@ -28,4 +28,5 @@ final remoteParameterFetcherProvider =
 );
 
 typedef RemoteParameterFetcherRef = ProviderRef<RemoteParameterFetcher>;
-// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
