445af2d587d99e2dcbcd53efd0428860cc2fc48d
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2023-09-08 11:31:14 +0900

    chore: regenerate files

--- packages/flutter_app/lib/pages/riverpod_example_page/async_notifier_provider_page/async_notifier_provider_page.g.dart
+++ packages/flutter_app/lib/pages/riverpod_example_page/async_notifier_provider_page/async_notifier_provider_page.g.dart
@@ -25,4 +25,4 @@ final asyncTodoListProvider =
 
 typedef _$AsyncTodoList = AutoDisposeAsyncNotifier<List<Todo>>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
