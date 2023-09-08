867597f79afa65ec480cc02e688a189e4be5dfda
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2023-09-08 11:36:02 +0900

    build: bump Riverpod to latest versions (#246)
    * build: bump Riverpod to latest versions
    
    * chore: remove unnecessary dependency overrides
    
    * chore: regenerate files

--- packages/flutter_app/lib/pages/riverpod_example_page/namespace_provider_page/namespace_provider_page.g.dart
+++ packages/flutter_app/lib/pages/riverpod_example_page/namespace_provider_page/namespace_provider_page.g.dart
@@ -54,4 +54,4 @@ final _livingPlaceProvider =
 
 typedef _$LivingPlace = AutoDisposeNotifier<String>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
