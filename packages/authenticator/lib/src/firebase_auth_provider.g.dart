dde8d63cacf78858d40679891bffc3efd92534a4
naipaka <naipakapaka@gmail.com>
2024-02-14 09:32:35 +0900

    chore: Execute auto-generation

--- packages/authenticator/lib/src/firebase_auth_provider.g.dart
+++ packages/authenticator/lib/src/firebase_auth_provider.g.dart
@@ -25,4 +25,4 @@ final firebaseAuthProvider = Provider<FirebaseAuth>.internal(
 
 typedef FirebaseAuthRef = ProviderRef<FirebaseAuth>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
