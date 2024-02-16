c2da20e0a3df3424c809a7580fce45ab14653906
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2024-02-16 21:35:11 +0900

    build: Bump Flutter SDK version to 3.19 with FVM (#336)
    * build: Bump Flutter to 3.19.0 with FVM 3.0
    
    * build: Update Firebase SDK version to 10.20.0
    Update CocoaPods version to 1.15.2
    
    * chore: Regenerate files with build_runner

--- packages/remote_parameter_fetcher/lib/src/remote_config_provider.g.dart
+++ packages/remote_parameter_fetcher/lib/src/remote_config_provider.g.dart
@@ -25,4 +25,4 @@ final remoteConfigProvider = Provider<FirebaseRemoteConfig>.internal(
 
 typedef RemoteConfigRef = ProviderRef<FirebaseRemoteConfig>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
