96cf7d930b0792fe134e2fbebfc3e215a3619c7d
naipaka <naipakapaka@gmail.com>
2024-02-19 12:33:01 +0900

    chore: Update generated files

--- packages/remote_parameter_fetcher/lib/src/parameters/is_under_maintenance_provider.g.dart
+++ packages/remote_parameter_fetcher/lib/src/parameters/is_under_maintenance_provider.g.dart
@@ -28,4 +28,4 @@ final isUnderMaintenanceProvider = Provider<bool>.internal(
 
 typedef IsUnderMaintenanceRef = ProviderRef<bool>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
