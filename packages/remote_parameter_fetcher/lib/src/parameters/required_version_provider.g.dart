c1e5ae660082b26773b6049e033a91cea33ca8b1
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2024-02-20 15:58:58 +0900

    chore: Add build diffs (#345)

--- packages/remote_parameter_fetcher/lib/src/parameters/required_version_provider.g.dart
+++ packages/remote_parameter_fetcher/lib/src/parameters/required_version_provider.g.dart
@@ -26,4 +26,4 @@ final requiredVersionProvider = Provider<Version>.internal(
 
 typedef RequiredVersionRef = ProviderRef<Version>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
