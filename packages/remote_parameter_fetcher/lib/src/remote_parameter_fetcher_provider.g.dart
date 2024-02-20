7dc0beb674a8e8760e301f9764acdcbe07b00345
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2024-02-20 14:46:44 +0900

    chore: Add build diffs

--- packages/remote_parameter_fetcher/lib/src/remote_parameter_fetcher_provider.g.dart
+++ packages/remote_parameter_fetcher/lib/src/remote_parameter_fetcher_provider.g.dart
@@ -29,4 +29,4 @@ final remoteParameterFetcherProvider =
 
 typedef RemoteParameterFetcherRef = ProviderRef<RemoteParameterFetcher>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
