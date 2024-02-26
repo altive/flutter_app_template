7a3b9a29c9bd9d5dbb536f8559cf245b9d1e6cfa
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2024-02-26 14:25:26 +0900

    build: Remove old internal packages

--- packages/authenticator/lib/src/firebase_auth_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'firebase_auth_provider.g.dart';
-
-/// FirebaseAuthのインスタンスを提供する
-@Riverpod(keepAlive: true)
-FirebaseAuth firebaseAuth(FirebaseAuthRef ref) => FirebaseAuth.instance;
--- packages/notification_receiver/lib/notification_receiver.dart
+++
@@ -1,8 +0,0 @@
-/// Notification receiver
-library notification_receiver;
-
-export 'package:firebase_messaging/firebase_messaging.dart'
-    show NotificationSettings;
-
-export 'src/notification_configurator_provider.dart';
-export 'src/notification_settings_extension.dart';
