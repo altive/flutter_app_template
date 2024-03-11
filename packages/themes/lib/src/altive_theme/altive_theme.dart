f6b6b1d82103483b859c53673d9baaadef73541f
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2024-03-11 13:56:46 +0900

    chore: Improve theme data and color scheme

--- packages/themes/lib/src/altive_theme/altive_theme.dart
+++ packages/themes/lib/src/altive_theme/altive_theme.dart
@@ -1,6 +1,6 @@
-export 'app_dark_theme_data.dart';
-export 'app_light_theme_data.dart';
+export 'dark_theme_data.dart';
 export 'layout.dart';
+export 'light_theme_data.dart';
 export 'theme_extensions/theme_extensions.dart';
 export 'utils/build_context_extension.dart';
 export 'utils/text_style_extension.dart';
