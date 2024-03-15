0f9ecc04450332f038be96e6a74c281fc2281c49
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2024-03-15 15:35:11 +0900

    chore: Update NavigationBarThemeData label behavior

--- packages/themes/lib/src/altive_theme/theme_data/navigation_bar_theme_data.dart
+++ packages/themes/lib/src/altive_theme/theme_data/navigation_bar_theme_data.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 NavigationBarThemeData navigationBarThemeData({
   required ColorScheme colorScheme,
 }) {
-  return const NavigationBarThemeData();
+  return const NavigationBarThemeData(
+    labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+  );
 }
