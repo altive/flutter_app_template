a2a246a4a1702f38294f1bfd135d950eeeb80846
Ryunosuke MURAMATSU <muramatsu@altive.co.jp>
2025-06-03 10:53:01 +0900

    fix: Organize build.yaml with GoRouter v15 (#580)

--- packages/pub_dev_api_client/lib/src/models/get_searched_packages_response_body.g.dart
+++ packages/pub_dev_api_client/lib/src/models/get_searched_packages_response_body.g.dart
@@ -8,16 +8,27 @@ part of 'get_searched_packages_response_body.dart';
 
 _GetSearchedPackagesResponseBody _$GetSearchedPackagesResponseBodyFromJson(
   Map<String, dynamic> json,
-) => _GetSearchedPackagesResponseBody(
-  packages: (json['packages'] as List<dynamic>)
-      .map((e) => PackageName.fromJson(e as Map<String, dynamic>))
-      .toList(),
-  nextPageUrl: json['next'] as String?,
+) => $checkedCreate(
+  '_GetSearchedPackagesResponseBody',
+  json,
+  ($checkedConvert) {
+    final val = _GetSearchedPackagesResponseBody(
+      packages: $checkedConvert(
+        'packages',
+        (v) => (v as List<dynamic>)
+            .map((e) => PackageName.fromJson(e as Map<String, dynamic>))
+            .toList(),
+      ),
+      nextPageUrl: $checkedConvert('next', (v) => v as String?),
+    );
+    return val;
+  },
+  fieldKeyMap: const {'nextPageUrl': 'next'},
 );
 
 Map<String, dynamic> _$GetSearchedPackagesResponseBodyToJson(
   _GetSearchedPackagesResponseBody instance,
 ) => <String, dynamic>{
-  'packages': instance.packages,
+  'packages': instance.packages.map((e) => e.toJson()).toList(),
   'next': instance.nextPageUrl,
 };
